129027a4fb71e22d5fc1a8a31518d94c910b8425
yunj <yunj8649@xingxingmobility.com>
2022-03-16 23:40:53 +0900

    모달 닫기

--- src/ImageInfo.js
+++ src/ImageInfo.js
@@ -23,13 +23,14 @@ class ImageInfo {
         const { id, name, url } = this.data.image;
         
         const response = await api.fetchCat(id);
+        if (!response) return;
         const { origin, temperament } = response.data;
   
         this.$imageInfo.innerHTML = `
           <div class="content-wrapper">
             <div class="title">
               <span>${name}</span>
-              <div class="close" onclick="onClose();">x</div>
+              <button class="close">x</button>
             </div>
             <img src="${url}" alt="${name}"/>        
             <div class="description">
@@ -38,10 +39,18 @@ class ImageInfo {
             </div>
           </div>`;
         this.$imageInfo.style.display = "block";
+        
+        this.$imageInfo.querySelector(".close").addEventListener("click", () => {
+          this.$imageInfo.style.display = "none";
+        });
+        // this.$imageInfo.addEventListener("keydown", e => {
+        //   if (e.keyCode === 27) {
+        //     this.$imageInfo.style.display = "none";
+        //   }
+        // });
       } else {
         this.$imageInfo.style.display = "none";
       }
-  
     }
   }
   
